52a98a6df7fb222f499c8fe993dabaeff309d72d
AliAshraf69420 <147829330+AliAshraf69420@users.noreply.github.com>
2024-08-25 11:35:49 +0300

    try 3

---
+++ StreamerApp/Hubs/MyHub.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Streamer.Models;
+using Streamer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streamer.Hubs
+{
+    public  class MyHub : Hub
+    {
+        private readonly IproductService _productService;
+        public MyHub(IproductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task SendOrder()
+        {
+            var product = _productService.Get(); // Fetch product using service
+            if (product != null)
+            {
+                // Send the product object to all connected clients
+                await Clients.All.SendAsync("ReceiveProduct", product);
+            }
+        }
+        public async Task<List<productClass>> GetProducts()
+        {
+            var product = _productService.Get();  // No need to await since Get() is synchronous
+
+            // Create a list with the single product (if needed)
+            var data = new List<productClass> { product };
+
+            return data.ToList();  // Return the list
+        }
+    }
+}
+
