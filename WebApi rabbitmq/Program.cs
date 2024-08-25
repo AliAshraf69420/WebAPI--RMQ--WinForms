58e81800f3e3ead550b8936ca634ab99afdeb7c8
AliAshraf69420 <147829330+AliAshraf69420@users.noreply.github.com>
2024-08-25 10:47:38 +0300

    please work

--- WebApi rabbitmq/Program.cs
+++ WebApi rabbitmq/Program.cs
@@ -38,7 +38,6 @@ namespace WebApi_rabbitmq
             }
 
             app.UseHttpsRedirection();
-            app.MapHub<MyHub>("/test");
             app.UseAuthorization();
             app.MapControllers();
 
